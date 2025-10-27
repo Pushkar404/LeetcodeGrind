3433bd770b30afaeb07bad0e3aede6d65a83c45f
Pushkar404 <pushkar8705@gmail.com>
2025-10-28 00:27:08 +0530

    Add twoSum function to find target indices
    Implement twoSum function to find indices of two numbers that add up to a target.

---
+++ 0001-Two_Sum.c
@@ -0,0 +1,16 @@
+int* twoSum(int* nums,int numSize, int target,  int* returnSize ){
+      *returnSize = 2;
+      int* ans = malloc(2*sizeof(int));
+      for (int i = 0; i<numSize; i++){
+            for (int j = i+1; j<numSize; j++){
+                  if (nums[i]+nums[j]==target){
+                        ans[0]=i;
+                        ans[1]=j;
+                        return ans;
+                  }
+            }
+      }
+      return NULL;
+      
+
+}
