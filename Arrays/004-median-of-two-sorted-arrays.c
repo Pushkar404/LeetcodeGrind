111d8a964878474955272aa467b16830fd3a12a4
Pushkar404 <puskar8705@gmail.com>
2025-10-30 17:35:23 +0530

    created merging function

--- Arrays/004-median-of-two-sorted-arrays.c
+++ Arrays/004-median-of-two-sorted-arrays.c
@@ -1,30 +1,27 @@
-int compare(const void *a,const void *b){
-    return (*(int *)a-*(int *)b);
+int* mergeArrays(int* nums1, int nums1Size, int* nums2, int nums2Size, int* mergedSize) {
+    *mergedSize = nums1Size + nums2Size;
+    int* merged = (int*)malloc((*mergedSize) * sizeof(int));
+    for (int i = 0; i < nums1Size; i++)
+        merged[i] = nums1[i];
+    for (int j = 0; j < nums2Size; j++)
+        merged[nums1Size + j] = nums2[j];
+
+    return merged;
 }
-
-double findMedianSortedArrays(int *nums1, int n1, int *nums2, int n2) {
-    int n = n1+n2;
-    int *merge = (int*)malloc(n*sizeof(int));
-    // Extend Array
-    for (int i=0; i<n1; i++){
-        merge[i]=nums1[i];
-    }
-    for (int i=0; i<n2; i++){
-        merge[i+n1]=nums2[i];
-    }
-    // Sorting Array
-    qsort(merge, n, sizeof(int), compare);
-
+int compare(const void *a, const void *b){
+    return *(int*)a - *(int*)b;
+}
+double findMedianSortedArrays(int* nums1, int nums1Size, int* nums2, int nums2Size) {
+    int mergedSize;
+    int* merged = mergeArrays(nums1, nums1Size, nums2, nums2Size, &mergedSize);
+    qsort(merged, mergedSize, sizeof(int), compare);
     double median;
-
-    if (n%2==1){
-        median = merge[n/2];
+    if (mergedSize%2==1){
+        median = merged[mergedSize/2];
     }
     else{
-        median = (merge[n/2]+merge[n/2 - 1])/2.0;
+        median = (merged[mergedSize/2] + merged[(mergedSize/2) - 1]) / 2.0;
     }
     free(merged);
-    return median;
-
-    
+    return median;    
 }
