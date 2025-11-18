f111138911d453a35271be53fb94c923b86e729a
Pushkar404 <pushkar8705@gmail.com>
2025-11-18 23:33:42 +0530

    Update 014-Longest-Common-Prefix.py

--- 014-Longest-Common-Prefix.py
+++ 014-Longest-Common-Prefix.py
@@ -1,1 +1,16 @@
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: str
+        """
+        prefix = strs[0]
 
+        for i in strs[1:]:
+
+            while not i.startswith(prefix):
+                prefix = prefix[:-1]
+
+                if prefix == "":
+                    return ""
+        return prefix   
